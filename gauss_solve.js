a4638ee8535104e879dc7d390a2d06ec21971c40
fastwomenandslowponies <fastwomenandslowponies@gmail.com>
2013-07-24 15:19:58 +0200

    Update gauss_solve.js

--- gauss_solve.js
+++ gauss_solve.js
@@ -1,5 +1,5 @@
- var gauss_solve = function(A){
-  var x = [], m = A.length, n = A[0].length;
+var gauss_solve = function(A){
+	var x = [], m = A.length, n = A[0].length;
 	for(var k = 0; k < m; k++){
 		var i_max = Number.NEGATIVE_INFINITY, i_max_val = Number.NEGATIVE_INFINITY; for(var i = k; i < m; i++)
 			if(Math.abs(A[i][k]) > i_max_val) i_max = i, i_max_val = Math.abs(A[i][k]);
@@ -10,12 +10,11 @@
 			A[i][k] = 0;
 		}
 	}
-
+	
 	for(var i = m - 1; i >= 0; i--){
 		x[i] = A[i][m]; for(var j = i + 1; j < m; j++) x[i] -= A[i][j]*x[j];
 		x[i] /= A[i][i];
 	}
-
+	
 	return x;
- };
- 
+};
