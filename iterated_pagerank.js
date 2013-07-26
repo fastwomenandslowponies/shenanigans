339f74cbd7a9158dbdc7b0a92c2efaef623d946b
fastwomenandslowponies <fastwomenandslowponies@gmail.com>
2013-07-26 08:46:39 +0200

    Update iterated_pagerank.js

--- iterated_pagerank.js
+++ iterated_pagerank.js
@@ -1,5 +1,5 @@
 var iterated_pagerank = function(A, d, iterations){
-  var p = [], L = [], m = A.length; for(var i = 0; i < m; i++){
+	var p = [], L = [], m = A.length; for(var i = 0; i < m; i++){
 		p[i] = 1.0, L[i] = 0; for(var j = 0; j < m; j++) if(A[i][j] > 0) L[i]++;
 	}
 	
@@ -8,6 +8,6 @@ var iterated_pagerank = function(A, d, iterations){
 		for(var i = 0; i < m; i++) for(var j = 0; j < m; j++) if(A[i][j] > 0) p_new[j] += d*p[i]/L[i];
 		p = p_new;
 	}
-
-  return p;
+	
+	return p;
 };
